aa271c350ef886a78bbbe4b4be2b79a2b2ad8640
Nicolas Lima <nicolaslima.dev.web@gmail.com>
2025-03-28 19:36:29 -0300

    feat: Conexão com o banco de dados postgree, criando o metodo GET e POST

--- src/main/java/com/crud/youtube/controllers/UserController.java
+++ src/main/java/com/crud/youtube/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.crud.youtube.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,10 +18,19 @@ public class UserController {
   
   @Autowired
   private UserRepository userRepository;
+
+  @PostMapping
   public ResponseEntity create(@RequestBody UserRequest userRequest){
     User user = new User(userRequest);
     userRepository.save(user);
 
     return ResponseEntity.ok( "salvo");
   } 
+
+  @GetMapping
+  public ResponseEntity listUser(){
+    var user = userRepository.findAll();
+
+    return ResponseEntity.ok(user);
+  }
 }
